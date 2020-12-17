6f2c64b346ec119917647d300fccec72a75df86c
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-12-17 10:16:52 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,5 @@
+
+        let a = new Date();
+        let date = a.toLocaleDateString();
+        let time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+        document.getElementById('time').innerHTML = time + "on" + date;
